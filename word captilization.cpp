c75aeaff4ef605db4b764abe5c52d58b3d449dc7
singhvarinder18148-max <singhvarinder18148@gmail.com>
2026-03-05 15:45:24 +0530

    Add files via upload

---
+++ word captilization.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+using namespace std;
+
+int main()
+{
+    string word;
+    cin >> word;
+    if (!word.empty())
+    {
+        word[0] = toupper(word[0]);
+    }
+    cout << word << endl;
+
+    return 0;
+}
