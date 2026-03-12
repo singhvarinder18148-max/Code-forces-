9a75db163df596b7c23738fdba2a709477ea5fa4
Varinder singh <singhvarinder18148@gmail.com>
2026-03-12 23:08:54 +0530

    Add files via upload

---
+++ beautiful year.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int y;
+    cin >> y;
+
+    while (true)
+    {
+        y++;
+        int a = y / 1000;
+        int b = (y / 100) % 10;
+        int c = (y / 10) % 10;
+        int d = y % 10;
+        if (a != b && a != c && a != d && b != c && b != d && c != d)
+        {
+            cout << y << "\n";
+            break;
+        }
+    }
+
+    return 0;
+}
